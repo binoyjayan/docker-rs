470f599806b55fcf3838752ba843ea7fc028adca
Binoy Jayan <binoyjayan@gmail.com>
2024-06-23 05:48:45 -0700

    wireup stdout and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use anyhow::{Context, Result};
+use std::io;
+use std::io::Write;
 
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
         .output()
         .with_context(|| {
             format!(
@@ -17,10 +18,12 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-    
+
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        let stdout = std::str::from_utf8(&output.stdout)?;
+        let stderr = std::str::from_utf8(&output.stderr)?;
+        io::stdout().write_all(stdout.as_bytes())?;
+        io::stderr().write_all(stderr.as_bytes())?;
     } else {
         std::process::exit(1);
     }
