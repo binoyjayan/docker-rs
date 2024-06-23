cbc5946cc898f0b56679b836018422a51b13fa73
Binoy Jayan <binoyjayan@gmail.com>
2024-06-23 06:18:56 -0700

    chroot into a directory

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use anyhow::{Context, Result};
-use std::io;
-use std::io::Write;
+use std::os::unix::fs::chroot;
+use std::{fs};
+use tempfile::tempdir;
 
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
+    let temp_dir = tempdir().context("Failed to create temporary directory")?;
+    let chroot_dir = temp_dir.path();
+    fs::create_dir_all(chroot_dir.join("usr/local/bin/")).context("failed to create /bin")?;
+    fs::create_dir_all(chroot_dir.join("dev/null")).context("failed to create /dev/null")?;
+    let dest = chroot_dir.join(command.strip_prefix("/").unwrap());
+    // Copy the 'docker-explorer' binary passed as 'args[3]'
+    std::fs::copy(command, dest)?;
+    chroot(chroot_dir).context("Failed to chroot")?;
+
     let output = std::process::Command::new(command)
         .args(command_args)
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
         .output()
         .with_context(|| {
             format!(
@@ -19,17 +30,6 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let stdout = std::str::from_utf8(&output.stdout)?;
-        let stderr = std::str::from_utf8(&output.stderr)?;
-        io::stdout().write_all(stdout.as_bytes())?;
-        io::stderr().write_all(stderr.as_bytes())?;
-    } else {
-        let exit_code = output.status.code().unwrap_or(1);
-        let stderr = std::str::from_utf8(&output.stderr)?;
-        io::stderr().write_all(stderr.as_bytes())?;
-        std::process::exit(exit_code);
-    }
-
-    Ok(())
+    let exit_code = output.status.code().unwrap_or(1);
+    std::process::exit(exit_code);    
 }
